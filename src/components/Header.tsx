b43a5aeb62df17c75b38782631f63f373b841652
Cheyne Hayworth <degenftm@gmail.com>
2023-12-10 15:18:26 +1100

    logo round

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,7 @@ const Header: React.FC<HeaderProps> = () => {
       <Link href="/" passHref> {/* Wrap the logo container with Link */}
               <div className={styles.logoContainer} onClick={closeAll}>
                 <Image
-                  src="/logoround.png"
+                  src="/twitter.png"
                   alt="CheyneDapp Logo"
                   height={48}
                   width={48}
